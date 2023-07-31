52ad725cd79436d1aa8ff85255360560edfb0854
Aedin Pittman <aedinpittman@gmail.com>
2023-07-31 15:18:01 -0500

    Venue HTML list function and event listener

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 import { bookingList } from "./bookings.js"
+import { venueList } from "./venues.js";
 
 let parentTag = document.querySelector(".booking-container");
-
 parentTag.innerHTML = bookingList();
+
+parentTag = document.querySelector(".venue-container")
+parentTag.innerHTML = venueList();
